7270af012e226647c352236709fbd5f08c089a96
GuzziMP <GuzziMP@web.de>
2015-12-19 13:02:09 +0100

    #428 Cleanup after move to github

--- Grabber_Interface/Merge/ilmerge.bat
+++ Grabber_Interface/Merge/ilmerge.bat
@@ -2,3 +2,4 @@
 IF EXIST MyFilms_Grabber_Interface_UNMERGED.exe del MyFilms_Grabber_Interface_UNMERGED.exe
 ren MyFilms_Grabber_Interface.exe MyFilms_Grabber_Interface_UNMERGED.exe
 ilmerge /out:MyFilms_Grabber_Interface.exe MyFilms_Grabber_Interface_UNMERGED.exe NLog.dll Grabber.dll  /target:dll /targetplatform:v4,C:\Windows\Microsoft.NET\Framework\v4.0.30319 /wildcards
+rem ilmerge /out:MyFilms_Grabber_Interface.exe MyFilms_Grabber_Interface_UNMERGED.exe NLog.dll Grabber.dll  /target:dll /targetplatform:"v4,%PROGS%\Reference Assemblies\Microsoft\Framework\.NETFramework\v4.0" /wildcards
